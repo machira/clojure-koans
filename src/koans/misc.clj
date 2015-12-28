e453f3d24c0f280754d1c212d8dc6833a5479621
Machira <rmachira@thoughtworks.com>
2015-12-28 11:27:12 -0800

    Raymond - partials not for the win. see: SO/a/15141076

--- src/koans/misc.clj
+++ src/koans/misc.clj
@@ -8,9 +8,15 @@
 (defn decmaker
   "returns a function that takes one argument and deducts y from it"
   [y]
-  (partial - y))
+  (fn [x] (- x y))
+  )
 
 (defn mapset
   "returns a set of the result of mapping func over the list"
   [fnc lst]
   (reduce #(into %1 (fnc %2)) #{} lst))
+
+(defn toInfix
+  "Takes a list like (1 + 3 * 4 - 5) and transforms it into lists that Clojure needs"
+  [lst]
+  ("as"))
--- test/koans/misc-test.clj
+++ test/koans/misc-test.clj
@@ -20,8 +20,25 @@
   (is (= -5 ((decmaker -5) -10)))
   )
 
-(deftest test-mapset
-  "should return a set of func'd things"
-  (= #{2 3} (mapset inc [1 1 2 2]))
-  (= '("HELLO WORLD" "DERP") (mapset #(.toUpperCase %) ["hello world" "derp"]))
+;(deftest test-mapset
+;  "should return a set of func'd things"
+;  (is (= #{2 3} (mapset inc [1 1 2 2])))
+;  (is (= '("HELLO WORLD" "DERP") (mapset #(.toUpperCase %) ["hello world" "derp"])))
+;  )
+
+(deftest test-toInfix
+  "should return an expected list"
+  (is (= '(+ 1 2) (toInfix ('(1 + 2)))))
+  ;(is (= '(+ 1 2) "derp"))
+  ;(is (= '(* 3 2) (toInfix ('(3 * 2)))))
+  ;(is (= '(+ 1 (+ 2 (* 3 4))) (toInfix ('(1 + 2 + 3 * 4)))))
+
   )
+
+
+(defmethod report :begin-test-ns [m]
+  (with-test-out
+    (if (some #(:test (meta %)) (vals (ns-interns (:ns m))))
+      (println "\nTesting" (ns-name (:ns m))))))
+
+(run-all-tests)
