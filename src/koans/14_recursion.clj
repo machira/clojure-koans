9d2e67e0d835a9e82e76ffa9304f688215d0bf7b
Machira <rmachira@thoughtworks.com>
2015-12-30 14:07:56 -0800

    Raymond - koans 14 recursion

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,22 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
-(defn recursive-reverse [coll]
-  __)
+(defn recursive-reverse
+  [coll]
+  (reduce conj '() coll))
 
 (defn factorial [n]
-  __)
+  (if (< n 2) 1 (if(> n 10000N) Integer/MAX_VALUE (* n (factorial (dec n))))))
 
 (meditations
   "Recursion ends with a base case"
